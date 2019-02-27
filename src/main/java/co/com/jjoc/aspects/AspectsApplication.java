f0baa42d8b9d37669a14e7d4525db0f22f6eb7f8
Juan Jose Orjuela Castillo <juan.orjuela@avaldigitallabs.com>
2019-02-27 11:36:14 -0500

    initial commit

---
+++ src/main/java/co/com/jjoc/aspects/AspectsApplication.java
@@ -0,0 +1,13 @@
+package co.com.jjoc.aspects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectsApplication.class, args);
+	}
+
+}
